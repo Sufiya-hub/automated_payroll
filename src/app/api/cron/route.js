464eaa4b04a0b1d2ad4fc6be8a39bd594a3e0286
Sufiya-hub <sufiyapattan4@gmail.com>
2025-03-08 21:53:52 +0530

    10:00pm

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { addDepartment } from '@/server/queries';
 
-export async function POST(req) {
+export async function GET() {
   try {
     const data = {
       departmentName: 'ECE',
