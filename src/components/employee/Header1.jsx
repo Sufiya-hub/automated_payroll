d9251c4ac81ec37e42cec795145c96c5be90a4d0
Kalyan5252 <kalyanpendem007@gmail.com>
2025-03-18 18:27:59 +0530

    tfjs fixed

--- src/components/employee/Header1.jsx
+++ src/components/employee/Header1.jsx
@@ -1,7 +1,6 @@
 'use client';
 import React, { useState } from 'react';
 import { signOut } from 'next-auth/react';
-import { div } from '@tensorflow/tfjs';
 import Navbar from './Navbar';
 import { IoSettingsOutline } from 'react-icons/io5';
 import { IoMdNotificationsOutline } from 'react-icons/io';
@@ -45,31 +44,3 @@ const Header1 = ({ attendanceBtn, setAttendanceDialog, empName }) => {
 };
 
 export default Header1;
-
-{
-  /* <div className="flex flex-col gap-4">
-      <div className="flex justify-between items-center">
-        <h1 className="font-normal text-3xl -mt-16">Welcome in, {empName}</h1>
-        <div className="flex gap-2">
-          {attendanceBtn && (
-            <button
-              type="button"
-              onClick={() => {
-                setAttendanceDialog(true);
-              }}
-              className="bg-brand font-bold text-white px-3 py-2 rounded-lg  shadow-md hover:bg-brand/80 transition-all"
-            >
-              Attendance
-            </button>
-          )}
-          {/* <button
-            type="button"
-            className="bg-red-500 font-bold text-white px-3 py-2 rounded-lg  shadow-md hover:bg-red-700 transition-all"
-            onClick={signOut}
-          >
-            Logout
-          </button> */
-}
-//     </div>
-//   </div>
-// </div> */}
