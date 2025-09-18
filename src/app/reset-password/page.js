23970482f88b571560333268cfe47dbfa70467e6
Sufiya-hub <sufiyapattan4@gmail.com>
2025-09-18 09:36:56 +0530

    reset

--- src/app/reset-password/page.js
+++ src/app/reset-password/page.js
@@ -1,4 +1,4 @@
-'use client';
+// 'use client';
 import React, { useMemo, useState } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 import { ToastContainer, toast } from 'react-toastify';
